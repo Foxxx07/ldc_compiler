f3a95bc71eb8a0bd960f9169cb66ec0d98c99b02
KMAit <kam.ait.ahmed@gmail.com>
2018-03-09 11:56:32 +0100

    keywords + gitignore

---
+++ parser/keywords.js
@@ -0,0 +1,12 @@
+const keywords = {
+  'string-declaraction': { r: /(String\s)/, s: 'String' },
+  'char-declaraction': { r: /(char\s)/, s: 'char' },
+  'int-declaraction': { r: /(int\s)/, s: 'int' },
+  'double-declaraction': { r: /(double\s)/, s: 'double' },
+  'byte-declaraction': { r: /(byte\s)/, s: 'byte' },
+  'boolean-declaraction': { r: /(boolean\s)/, s: 'boolean' },
+  'float-declaraction': { r: /(float\s)/, s: 'float' },
+  'console-object': { r: /(System)/, s: 'System' }
+};
+
+module.exports = keywords;
