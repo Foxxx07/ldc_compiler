ceec98a7127a75fe0220ac48006750ec74171d78
KMAit <kam.ait.ahmed@gmail.com>
2018-03-09 14:46:24 +0100

    keywords + gitignore

--- parser/keywords.js
+++ parser/keywords.js
@@ -1,12 +1,22 @@
 const keywords = {
-  'string-declaraction': { r: /(String\s)/, s: 'String' },
-  'char-declaraction': { r: /(char\s)/, s: 'char' },
-  'int-declaraction': { r: /(int\s)/, s: 'int' },
-  'double-declaraction': { r: /(double\s)/, s: 'double' },
-  'byte-declaraction': { r: /(byte\s)/, s: 'byte' },
-  'boolean-declaraction': { r: /(boolean\s)/, s: 'boolean' },
-  'float-declaraction': { r: /(float\s)/, s: 'float' },
-  'console-object': { r: /(System)/, s: 'System' }
+    'string-declaraction': { r: /(String\s)/, s: 'String' },
+    'char-declaraction': { r: /(char\s)/, s: 'char' },
+    'int-declaraction': { r: /(int\s)/, s: 'int' },
+    'double-declaraction': { r: /(double\s)/, s: 'double' },
+    'byte-declaraction': { r: /(byte\s)/, s: 'byte' },
+    'boolean-declaraction': { r: /(boolean\s)/, s: 'boolean' },
+    'float-declaraction': { r: /(float\s)/, s: 'float' },
+    'short-declaraction': { r: /(short\s)/, s: 'short' },
+    'long-declaraction': { r: /(long\s)/, s: 'long' },
+    'console-object': { r: /(System)/, s: 'System' },
+    'public-accessor': { r: /(public\s)/, s: 'public' },
+    'private-accessor': { r: /(private\s)/, s: 'private' },
+    'protected-accessor': { r: /(protected\s)/, s: 'protected' },
+    'static-element': { r: /(static\s)/, s: 'static' },
+    'while-condition': { r: /(while\s*\(+\s?)/, s: 'while(' },
+    'if-condition': { r: /(if\s*\(+\s?)/, s: 'if' },
+    'class': { r: /(class\s)/, s: 'class' }
+
 };
 
 module.exports = keywords;
